51a3ff61df6f53e18712ec92d4238a15139f717d
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-01 22:22:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,10 @@ def monopoly_with_third_tier
   
   monopoly = {
 	  :railroads => {
-	    :pieces => 4, :rent_in_dollars => {}
+	    :pieces => 4, :rent_in_dollars => {
+	      :one_piece_owned => 25,
+	      
+	    }
 	  }
   }
   
